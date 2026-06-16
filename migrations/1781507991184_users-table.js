c206fa0368437bf663ed9b2d43c0033f34812143
moeen <mashruf125@gmail.com>
2026-06-16 14:05:59 +0600

    feat: create users table migration with status enum and tenant constraints

--- migrations/1781507991184_users-table.js
+++ migrations/1781507991184_users-table.js
@@ -1,3 +1,6 @@
+import { formatEnumForSql } from '../src/common/utils/sql-builder.util';
+import { UserStatus } from '../src/users/enums/users.enums';
+
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
@@ -10,6 +13,10 @@ export const shorthands = undefined;
  */
 export const up = (pgm) => {
   pgm.sql(`
+    CREATE TYPE user_status AS ENUM (${formatEnumForSql(UserStatus)})
+    `);
+
+  pgm.sql(`
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             tenant_id INT NOT NULL REFERENCES tenants(id) ON DELETE CASCADE,
@@ -17,7 +24,7 @@ export const up = (pgm) => {
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL,
             phone VARCHAR(11) UNIQUE NOT NULL CHECK (LENGTH(phone) = 11),
-            status VARCHAR(20) NOT NULL DEFAULT 'active',
+            status user_status NOT NULL DEFAULT 'active',
             role VARCHAR(20) NOT NULL REFERENCES roles(title) ON DELETE CASCADE,
             refresh_token VARCHAR(255),
             created_at DATE DEFAULT CURRENT_DATE
