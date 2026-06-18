e80013cf4fbbd4c883282da47627a3aa7d4ceb41
moeen <mashruf125@gmail.com>
2026-06-18 11:27:59 +0600

    feat: initialize products, users, auth, and warehouses feature modules

--- src/warehouses/warehouses.module.ts
+++ src/warehouses/warehouses.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WarehousesService } from './warehouses.service';
 import { WarehousesController } from './warehouses.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [WarehousesController],
   providers: [WarehousesService],
   exports: [WarehousesService],
